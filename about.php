acc271334fc743bce7c0a82ea3398feac5b1a3e9
Om-Prakash1999 <omprakashGudz1999@gmail.com>
2026-03-15 19:50:58 +0530

    Added about section and some photos

---
+++ about.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Page</title>
+
+
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300&family=Montserrat&family=Poppins:wght@600&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="stylemainpage.css">
+    <link rel="stylesheet" href="about.css">
+
+    
+</head>
+<body>
+<nav>
+    <div class="container nav_container">
+        <a href="welcome.php"><h4>ICMSE 2024</h4></a>
+        <ul class="nav_menu">
+            <li><a href="committee.php">Committee</a></li>
+            <li><a href="registration.php">Registration</a></li>
+            <li><a href="programme.php">Programme</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            </ul>
+        <a href="logout.php" class="btn btn-primary">Log Out</a>
+        <button id="open-menu-btn"><i class="uil uil-bars"></i></button>
+        <button id="open-menu-btn"><i class="uil uil-multiply"></i></button>
+    </div>
+</nav>
+
+<section class="about_achievements">
+    <div class="container about_achievements-container">
+        <div class="about_achievements-left">
+          <br><br><br><br><br><br><br><img src="aboutobj.svg">
+    </div>
+
+    <div class="about_achievements-right">
+        <h2>About ICMSE 2024</h2><br>
+        <b><p align="justify">
+        International Conference on ‘Materials for Sustainable Environment (ICMSE-2024)’ is a forum for directing the knowledge and tools of science and technology 
+        of advanced materials to contribute to solve major problems of the society. The drive for sustainability in materials science and technology offers new possibilities
+         in physics, chemistry, nanofluids, electronics, materials science, energy, manufacturing industries and biological sciences including medicine and agriculture to 
+         mitigate the undesirable environmental impacts from technology and economic growth.  The efficient use of materials ensures in the formation of a healthy and safe
+         environment.</p><p></p><br>
+        <p align= "justify"> Many technologies in the materials currently providing important benefits to humanity cannot continue indefinitely and must be directed towards a more 
+       sustainable path as sustainable development is the development that meets the needs of the present without compromising the ability of the future generations to meet
+       their own needs. Sustainable development is a huge field that capture the concepts of environmental stewardship, material management, green manufacturing, renewable 
+       and clean energy technologies, and water and air management under one tent.
+        </p></b>
+    </div>
+</section>
+
+
+<section class="about_achievements">
+    <div class="container about_achievements-container">
+        <div class="about_achievements-left">
+        <h2>Objectives of the Conference</h2><br>
+        <b><p align= "justify">The main aim of this conference is to bring together leading academic scientists, researchers, students, academicians and industry persons to exchange and
+        share their experiences and research results in all aspects of materials utilization for sustainability of our environment.</p><br> <p align="justify">
+        Recent researches on Materials for Sustainable Environment provide a premier interdisciplinary platform for researchers and educators, 
+        to present and discuss most recent innovations, trends and concerns.</p> <br><p align= "justify">It also investigates upon the practical challenges encountered and solutions 
+        adopted in the field of electronic, magnetic, optical, dielectric as well as biological fields. <br><p align= "justify">In order to meet the needs of future, scientists/researchers 
+        are actively involved to optimize wide varieties of materials.</p> <br><p align= "justify">Objective of the present conference is to bring together leading academician, scientists, 
+        researchers and industry persons to exchange and share their scientific knowledge.</p><p></p><br>
+        <p align= "justify">The conference consists of a combination of symposia of basic and multidisciplinary science, and applied symposia focused on Sustainable Development areas.   
+</p></b>
+    </div>
+
+    <div class="about_achievements-right">
+       <br><br><br><br><br><br><br><br><br> <img src="aboutcollege.svg">
+    </div>
+</section>
+
+
+
+<footer>
+    <div class="container footer_container">
+        <div class="footer_1">
+            <a href="welcome.php" class="footer_logo"><h4>ICMSE 2024</h4></a>
+        </div>
+        <div class="footer_2">
+            <h4>Permalinks</h4>
+            <ul class="permalinks">
+            <li><a href="committee.php">Committee</a></li>
+            <li><a href="registration.php">Registration</a></li>
+            <li><a href="programme.php">Programme</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+
+        <div class="footer_3">
+            <h4>Primacy</h4>
+            <ul class="privacy">
+                <li><a href="privacy.php">Privacy policy</a></li>
+                <li><a href="term.php">Terms and conditions</a></li>
+            </ul>
+        </div>
+
+        <div class="footer_4">
+
+        <div class="footer_copyright">
+            <small>Copyright &copy; ICMSE 2024 </small><br>
+            <small>Made By : <br> Om Prakash Mohanty - 200301120012<br> Shibam Acharya - 200301120022 </small>
+        </div>
+        </div>
+   </div>
+</footer>
+
+
+    <script src="./mainpage.js"></script>
+
+</body>
+</html>
