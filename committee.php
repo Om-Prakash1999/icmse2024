e6e682808476cf871a20de3860f4709b4ae6edbc
Om-Prakash1999 <omprakashGudz1999@gmail.com>
2026-03-16 17:05:12 +0530

    Added some files

---
+++ committee.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Committee Page</title>
+
+
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300&family=Montserrat&family=Poppins:wght@600&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="stylemainpage.css">
+    <link rel="stylesheet" href="committee.css">
+
+    
+</head>
+<body>
+<nav>
+    <div class="container nav_container">
+        <a href="welcome.php"><h4>ICMSE 2024</h4></a>
+        <ul class="nav_menu">
+            <li><a href="committee.php">Committee</a></li>
+            <li><a href="registration.php">Registration</a></li>
+            <li><a href="programme.php">Programme</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            </ul>
+        <a href="logout.php" class="btn btn-primary">Log Out</a>
+        <button id="open-menu-btn"><i class="uil uil-bars"></i></button>
+        <button id="open-menu-btn"><i class="uil uil-multiply"></i></button>
+    </div>
+</nav>
+
+    <section class="about_achievements">
+   
+    <center><h1>Committee</h1><br><br>
+    <center><p align = "justify">Organizing committee play a pivotal role in coordinating and executing successful events, projects, or initiatives. These committee serve as the
+         backbone of collaborative efforts, bringing together individuals with diverse skills, perspectives, and expertise to achieve a common goal. Whether 
+         planning a community event, managing a business project, or overseeing a charitable initiative, a well-structured organizing committee can make all the difference.
+
+Effective organization within a committee involves clear roles and responsibilities, ensuring that each member contributes to the overall success of the endeavor. 
+Assigning specific tasks based on individual strengths fosters a sense of ownership and accountability. Regular communication and transparent decision-making processes 
+are essential to keep everyone informed and engaged.
+
+Furthermore, diversity within a committee enhances creativity and problem-solving by incorporating a variety of perspectives. Well-defined leadership, such as a 
+chairperson or project manager, helps streamline decision-making and maintain focus on the overarching objectives.
+
+Successful organizing committee also prioritize flexibility, adapting to unforeseen challenges and changes. Regular meetings, progress updates, and feedback sessions 
+contribute to a dynamic and responsive working environment.
+
+In essence, organizing committee are the engines that drive collaborative efforts. When structured effectively, these committee can transform visions into reality, 
+making them indispensable in achieving collective goals across various domains.</p>
+
+
+  </section> 
+       
+
+<section class="team"> 
+       <h2>Organising Committee</h2>
+       <div class="container team_container">
+        <article class="team_member">
+            <div class="team_member-image">
+                <img src="supriyamam.jpg" height=350px width=350px>
+            </div>
+            <div class="team_member-info">
+                <h4>Dr. Supriya Pattanayak</h4>
+                <h5>PATRON</h5>
+                <p>Vice Chancellor<br>CUTM,Odisha,India</p>
+            </div>
+        </article>
+
+        <article class="team_member">
+            <div class="team_member-image">
+                <img src="biswajitsir.jpeg" height=350px width=350px>
+            </div>
+            <div class="team_member-info">
+                <h4>Dr. Biswajit Mishra</h4>
+                <h5>CO-PATRON</h5>
+                <p>Pro-Vice Chancellor<br>CUTM,Odisha,India</p>
+            </div>
+        </article>
+
+        <article class="team_member">
+            <div class="team_member-image">
+                <img src="susantasir.jpeg" height=350px width=350px>
+            </div>
+            <div class="team_member-info">
+                <h4>Dr.Susanta Kumar Biswal</h4>
+                <h5>CHAIRMAN</h5>
+                <p>Professor, Department of Chemistry<br>CUTM,Odisha,India</p>
+            </div>
+        </article>
+</div>
+</section>
+
+
+<div class="container footer_container">
+<div class="footer_1"><br><br>
+    <h3>Conveners</h3><br>
+    <ul>
+    <li>Dr. Yashaswi Nayak</li>
+        <li>Dr. Mohammed Siddique</li>
+        
+    </ul><br><br>
+</div>
+
+<div class="footer_2"><br><br>
+    <h3>Co-Conveners</h3><br>
+    <ul>
+        <li>Dr. Asish Kumar Sahoo</li>
+        <li>Dr. Santosh Kumar Satpathy</li>
+        <li>Dr. Rukmini Mishra</li>
+        <li>Dr. Gyanranjan Mahalik </li>
+        <li>Dr. Goutam Kumar Mahato</li>
+        <li>Dr. Narayan Gouda</li>
+        <li>Dr. Srikanta Maharana</li>
+    </ul><br><br>
+</div>
+
+<div class="footer_3"><br><br>
+    <h3>Organizing Committee</h3><br>
+    <ul>
+        <li>Dr. Dojalisa Sahu</li>
+            <li>Dr. Sagarika Parida</li>
+            <li>Dr. Chittaranjan Routray </li>
+            <li>Dr. Soubhagyalaxmi Behera</li>
+    </ul>
+
+</div>
+<style>
+#more {display: none}
+</style>
+<div class="footer_4"><br><br>
+    <h3>International Advisory Committee</h3><br>
+    <ul>
+        <li>Dr I-Ming Hung, Yuanze University,Taiwan</li>
+        <li>Dr. Cicero Cena, Insituteo de Fisica, Campo Grande-MS, Brasil</li>
+            <li>Dr. Ramanuj Narayan, Director, CSIR-IMMT, India</li>
+            <li>Dr. Ashok Mishra,IIT Madras, India</li><span id="dots">...</span>
+            <span id="more"><li>Dr. K K Nanda,IOP, Bhubaneswar, India</li>
+            <li>Dr. KC Gupta,IIT Roorkee, India</li>
+            <li>Dr. Sihartha Panda, IIT Kanpur, India</li>
+            <li>Dr. Shalini Kumari, The Pennsylvania State University, University Park, Pennsylvania 16802, USA</li>
+            <li>Dr. Nagendra Parasad Yadav, Hubei Polytechnic University Huangshi, China</li>
+            <li>Dr. Ram Gupta, Pittsburg State University, USA</li>
+            <li> Yassine Slimani, Imam Abdulrahman Bin Faisal University, Saudi Arabia</li>
+            <li>Siva K.P. Sivakumaran, Centre for Aquatic and Fish Ecology Research, Melbourne, Australia</li>
+            <li> Asis Behura, Princess Royal Hospital, UK</li>
+            <li>Dr. Mohammed Naimuddin, Adama Science and Technology University, Adama, Ethiopia</li>
+            <li>Dr. Felix Kaspar, Technical University of Berlin, Germany</li></span>
+            <button onclick="myFunction()" id="myBtn">Read more</button>
+    </ul>
+</div>
+
+<div class="footer_5"><br><br>
+    <h3>National Advisory Committee</h3><br>
+    <ul>
+        <li>Dr. Sujit Roy,IIT Bhubaneswar</li>
+            <li>Dr. Jyoti Kumar, Ranchi University, India</li>
+            <li>Dr. Annaian Shanmugam, Annamalai University, Tamilnadu, India</li>
+            <li>Dr. Algari Thivakaran, Blue Bay Coastal Research Foundation, Tamilnadu, India</li>
+            <li>Sri Sanath Kumar N., Nandankanan Zoological Park, Odisha, India</li>
+            <li>Dr. K.L. Tiwari, Ravi Sankar University, Raipur, CG, India</li>
+            <li>Dr. Pradeep Kumar Chand, North Orissa University, Odisha, India</li>
+            <li>Dr. Biswajit Mukherjee, Jadavpur University, Kolkata</li>
+            <li>Dr Rajkishore Patel,  NIT, Rourkela</li> 
+            <li>Dr Tarashankar Pal, IIT Kharagpur</li> 
+            <li>Dr. Anil Mohapatra, Zoological Survey of India, Gopalpur, Odisha, India</li>
+            <li>Dr. Basudev Tripathy, Zoological Survey of India, Pune, India</li>
+            <li>Dr. B.K. Saren, Visva-Bharati, Sriniketan, West Bengal</li>
+            <li>Dr. Biswajit Pramanick, RPCAU, Samastipur, Bihar</li>
+            <li>Dr. Partendu Poddar, Uttar Banga Krishi Viswavidyalaya, Coochbehar, West Bengal</li>
+    </ul>
+</div>
+
+<div class="footer_6"><br><br>
+    <h3>Local Organizing Committee</h3><br>
+    <ul>
+        <li>Dr Rama Chandra Mohanty, Utkal University, Odisha, India</li>
+            <li>Dr Kunjabihari Satpathy, Utkal University, Odisha, India</li>
+            <li>Dr Puspanjali Parida, North-Odisha University</li>
+            <li>Dr Nabin Kumar Dhala, CSIR-IMMT</li>
+            <li>Dr Chinmay Pradhan, Utkal University, Odisha, India</li>
+            <li>Dr Arup Mukharjee, NRRI, Cuttack, Odisha</li>
+            <li>Dr Rati Ranjan Nayak, CSIR-IICT, India</li>
+            <li>Dr Raj Kumar Joshi, Ramadevi University, Odisha</li> 
+            <li>Dr Ashok Mishra</li>
+            <li>Dr Subrat Sarangi</li>
+            <li>Dr Padmaja Pattnaik</li>
+            <li>Dr Nibedita Nayak</li>
+            <li>Dr. Tumbanath Samantara</li>
+            <li>Dr. Narayan Gouda</li>
+            <li>Dr. Pratap Chhotaray</li>
+            <li>Dr. Suchismita Acharya</li> 
+            <li>Dr. Shraban Kumar Sahoo</li> 
+            <li>Dr. Subhraraj Panda</li> 
+            <li>Dr. Prasanta Kumar Rath</li> 
+            <li>Dr. Satyanarayan Dhal</li>
+            <li>Dr. D Kumar Mohanty</li>
+            <li>Dr. Asish Kumar Sarangi</li> 
+            <li>Dr. Rabindra Kumar Pradhan</li>
+            <li>Mr. Chittaranjan Pattnayak</li>
+            <li>Mr. Subashis Routray</li>
+            <li>Mr. Nrusinha Das</li>
+            <li>Mr. Jogesh Chandra Mishra</li><br><br>
+    </ul>
+
+</div>
+
+</div>
+
+<footer>
+    <div class="container footer_container">
+        <div class="footer_1">
+            <a href="welcome.php" class="footer_logo"><h4>ICMSE 2024</h4></a>
+        </div>
+        <div class="footer_2">
+            <h4>Permalinks</h4>
+            <ul class="permalinks">
+            <li><a href="committee.php">Committee</a></li>
+            <li><a href="registration.php">Registration</a></li>
+            <li><a href="programme.php">Programme</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+
+        <div class="footer_3">
+            <h4>Primacy</h4>
+            <ul class="privacy">
+                <li><a href="privacy.php">Privacy policy</a></li>
+                <li><a href="term.php">Terms and conditions</a></li>
+            </ul>
+        </div>
+
+        <div class="footer_4">
+
+        <div class="footer_copyright">
+            <small>Copyright &copy; ICMSE 2024 </small><br>
+            <small>Made By : <br> Om Prakash Mohanty - 200301120012<br> Shibam Acharya - 200301120022 </small>
+        </div>
+        </div>
+   </div>
+</footer>
+
+
+    <script src="./mainpage.js"></script>
+    <script>
+        function myFunction() {
+          var dots = document.getElementById("dots");
+          var moreText = document.getElementById("more");
+          var btnText = document.getElementById("myBtn");
+        
+          if (dots.style.display === "none") {
+            dots.style.display = "inline";
+            btnText.innerHTML = "Read more"; 
+            moreText.style.display = "none";
+          } else {
+            dots.style.display = "none";
+            btnText.innerHTML = "Read less"; 
+            moreText.style.display = "inline";
+          }
+        }
+        </script>
+
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Page</title>
+
+
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300&family=Montserrat&family=Poppins:wght@600&display=swap" rel="stylesheet">
+
+
+    <!--Swiper JS-->
+    <link
+  rel="stylesheet"
+  href="https://unpkg.com/swiper@8/swiper-bundle.min.css"
+/>
+
+    <link rel="stylesheet" href="stylemainpage.css">
+    <link rel="stylesheet" href="contact.css">
+</head>
+<body>
+<nav>
+    <div class="container nav_container">
+        <a href="welcome.php"><h4>ICMSE 2024</h4></a>
+        <ul class="nav_menu">
+            <li><a href="committee.php">Committee</a></li>
+            <li><a href="registration.php">Registration</a></li>
+            <li><a href="programme.php">Programme</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            </ul>
+        <a href="logout.php" class="btn btn-primary">Log Out</a>
+        <button id="open-menu-btn"><i class="uil uil-bars"></i></button>
+        <button id="open-menu-btn"><i class="uil uil-multiply"></i></button>
+    </div>
+</nav>
+
+
+
+
+    <section class="contact">
+      <div class="container contact_container">
+        <aside class="contact_aside">
+          <div class="aside_image">
+            <img src="contact.svg">
+          </div>
+          <h3>Contact Us</h3><p>
+          <p>
+            We are here to help you.</p>
+          <ul class ="contact_details">
+            <li>
+            <i class ="uil uil-user"></i>
+            <h5>Dr. Susanta Kumar Biswal</h5>
+            </li>
+            <li>
+              <i class ="uil uil-phone-times"></i>
+              <h5>+91 9438607080/ +91 9438807060</h5>
+            </li>
+
+            <li>
+              <i class ="uil uil-envelope"></i>
+              <h5>icmse2024@cutm.ac.in </h5>
+            </li>
+            <br>
+            </li>
+            <li>
+                <i class ="uil uil-user"></i>
+                <h5>Dr. Yashaswi Nayak</h5>
+            </li>
+            <li>
+                 <i class ="uil uil-phone-times"></i>
+                 <h5>+9198615 22222</h5>
+            </li>
+            <li>
+                  <i class ="uil uil-envelope"></i>
+                  <h5>icmse2024@cutm.ac.in </h5>
+            </li>
+        </ul>
+
+          <ul class="contact_socials">
+            
+            <li> <a href="mailto:icmse2024@cutm.ac.in" target="_blank"><i class="uil uil-envelope"></i></a></li>
+            <li> <a href="https://cutm.ac.in/" target="_blank"><i class="uil uil-globe"></i></a></li>
+          </ul>
+        </aside>
+        <div><img src="contact.png"></div>
+        
+      </div>
+      
+    </section>
+
+
+    <footer>
+    <div class="container footer_container">
+        <div class="footer_1">
+            <a href="welcome.php" class="footer_logo"><h4>ICMSE 2024</h4></a>
+        </div>
+        <div class="footer_2">
+            <h4>Permalinks</h4>
+            <ul class="permalinks">
+            <li><a href="committee.php">Committee</a></li>
+            <li><a href="registration.php">Registration</a></li>
+            <li><a href="programme.php">Programme</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+
+        <div class="footer_3">
+            <h4>Primacy</h4>
+            <ul class="privacy">
+                <li><a href="privacy.php">Privacy policy</a></li>
+                <li><a href="term.php">Terms and conditions</a></li>
+            </ul>
+        </div>
+
+        <div class="footer_4">
+
+        <div class="footer_copyright">
+            <small>Copyright &copy; ICMSE 2024 </small><br>
+            <small>Made By : <br> Om Prakash Mohanty - 200301120012<br> Shibam Acharya - 200301120022 </small>
+        </div>
+        </div>
+   </div>
+</footer>
+
+
+    <script src="mainpage.js"></script>
+
+</body>
+</html>
