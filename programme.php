922accc93190b9526e6f037434be66509d4b0580
Om-Prakash1999 <omprakashGudz1999@gmail.com>
2026-03-19 17:46:44 +0530

    added

---
+++ programme.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Programme Page</title>
+
+
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300&family=Montserrat&family=Poppins:wght@600&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="stylemainpage.css">
+    <link rel="stylesheet" href="about.css">
+
+    
+</head>
+<body>
+<nav>
+    <div class="container nav_container">
+        <a href="welcome.php"><h4>ICMSE 2024</h4></a>
+        <ul class="nav_menu">
+            <li><a href="committee.php">Committee</a></li>
+            <li><a href="registration.php">Registration</a></li>
+            <li><a href="programme.php">Programme</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            </ul>
+        <a href="logout.php" class="btn btn-primary">Log Out</a>
+        <button id="open-menu-btn"><i class="uil uil-bars"></i></button>
+        <button id="open-menu-btn"><i class="uil uil-multiply"></i></button>
+    </div>
+</nav>
+
+    <section class="about_achievements">
+      <div class="container about_achievements-container">
+        <div class="about_achievements-left">
+          <br><br><br><br><br><img src="programme.svg">
+</div>
+
+<div class="about_achievements-right">
+  <h1>Programme</h1><br>
+
+  <br><h3>Important Dates</h3><br>
+                 <p><b> Submission of Abstracts :</b> 25th January , 2024 onwards<br><br>
+                 <a href="https://forms.gle/8Co9Qm72EfzgxyjPA" target="_blank" class="btn btn-primary">Submit Abstract Here</a> <br><br>
+                 <b>Notification of Acceptance of Abstract:</b> 15th Frebruary, 2024<br>
+                 <b>Submission of Full Paper :</b> 25th March, 2024 - 10th April, 2024<br>
+                 <b>Last Date for Registration :</b> 25th February,2024<br>
+         </p><br>
+
+         </p></b>
+         <h3>Paticipants may register online at - </h3><br>
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSfhFY5zSScrn4vJXLwrsgyUWeskODzl_mKDdC8dR6g6qzHyTQ/viewform" target="_blank" class="btn btn-primary">Register Here</a> <br><br>
+
+<h3>Programme Schedule</h3><br>
+<div class="achievements_cards">
+     <article class="achievement_card">
+        <span class="achievement_icon">
+        <i class="uil uil-video"></i>
+        </span>
+        <h3>---</h3>
+        <p>Day 1</p>
+        <p>(Updated Soon)</p>
+     </article>
+
+     <article class="achievement_card">
+        <span class="achievement_icon">
+        <i class="uil uil-users-alt"></i> 
+        </span>
+        <h3>---</h3>
+        <p>Day 2</p>
+        <p>(Updated Soon)</p>
+</article>
+
+     <article class="achievement_card">
+        <span class="achievement_icon">
+        <i class="uil uil-trophy"></i>
+            
+        </span>
+        <h3>---</h3>
+        <p>Day 3</p>
+        <p>(Updated Soon)</p>
+     </article>
+</div> </div>
+</section>
+
+
+<footer>
+    <div class="container footer_container">
+        <div class="footer_1">
+            <a href="welcome.php" class="footer_logo"><h4>ICMSE 2024</h4></a>
+        </div>
+        <div class="footer_2">
+            <h4>Permalinks</h4>
+            <ul class="permalinks">
+            <li><a href="committee.php">Committee</a></li>
+            <li><a href="registration.php">Registration</a></li>
+            <li><a href="programme.php">Programme</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+
+        <div class="footer_3">
+            <h4>Primacy</h4>
+            <ul class="privacy">
+                <li><a href="privacy.php">Privacy policy</a></li>
+                <li><a href="term.php">Terms and conditions</a></li>
+            </ul>
+        </div>
+
+        <div class="footer_4">
+
+        <div class="footer_copyright">
+            <small>Copyright &copy; ICMSE 2024 </small><br>
+            <small>Made By : <br> Om Prakash Mohanty - 200301120012<br> Shibam Acharya - 200301120022 </small>
+        </div>
+        </div>
+   </div>
+</footer>
+
+    <script src="./mainpage.js"></script>
+
+</body>
+</html>
